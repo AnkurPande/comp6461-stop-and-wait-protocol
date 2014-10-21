924c980673947206ffc6caf1bb1c2f111c55ac09
ankur.pandey111@gmail.com <ankur.pandey111@gmail.com@aba4ceb9-df21-aab8-e31d-ca1b3566f537>
2014-10-21 20:20:30 +0000

    Added UDPClient class with its data and member functions declaration.

--- UDPClient/UDPClient/Client.h
+++ UDPClient/UDPClient/Client.h
@@ -14,9 +14,12 @@
 #define MAX_RANDOM 256 //Upper limit for random numbers
 #define SEQUENCE_WIDTH 1 // bits width for sequence in stop and wait protocol
  
+#include <winsock.h>
+#include<fstream>
 using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
 
-typedef enum{GET=1,PUT,DELETE,LIST}Direction; 
+typedef enum{GET=1,PUT,DEL,LIST}Direction; 
 typedef enum{HANDSHAKE=1,FRAME,FRAME_ACK}PacketType ;
 typedef enum{TIMEOUT=1,INCOMING_PACKET,RECIEVE_ERROR}RecieveResult;
 typedef enum{CLIENT_REQ=1,ACK_CLIENT_NUM,ACK_SERVER_NUM,FILE_NOT_EXIST,INVALID}HandshakeType;
@@ -46,3 +49,33 @@ typedef struct {
 	char buffer[MAX_FRAME_SIZE];
 	int buffer_length;
 }MessageFrame;
+
+class UDPClient{
+	int sock;
+	SOCKADDR_IN sa;			// client info, IP, port 5000
+	SOCKADDR_IN sa_in;		// router info, IP, port 7000
+	int sa_in_size;
+	timeval timeouts;
+	ThreeWayHandshake handshake;
+	int random;
+	WSADATA wsadata;
+	
+private:
+	std::ofstream fout;
+
+public:
+	UDPClient(char * fn = "ClientLogFile.txt"); // constructor
+	~UDPClient(); //Destructor
+	
+	void run();
+	bool sendFile(int, char *, char *,int); //Send the file after the hanshake succeeded
+	int sendRequest(int, ThreeWayHandshake *, SOCKADDR_IN *); //Start the initiation of hanshake
+	int sendFrame(int, MessageFrame *); // Send the frame of data
+	int sendFileAck(int, Acknowledgement *); // Send the File Acknowledgment
+
+	bool recieveFile(int, char *, char *, int); // Recieve the File from remote host after the handshake
+	RecieveResult ReceiveResponse(int, ThreeWayHandshake *); // Recieve the response after the initiation of handshake  
+	RecieveResult ReceiveFrame(int, MessageFrame *); // Recieve Frame of data
+	RecieveResult ReceiveFileAck(int, Acknowledgement *); // Recieve File Acknowledgment 
+
+};
